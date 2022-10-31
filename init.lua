5f79dce35385e38038c7f92d03e80461814e7c99
Eric Anderson <eric@bloodhub.com>
2022-10-31 10:57:11 -0400

    Some ruby-based snippets
    I really need to evaluate what the Ruby plugin is doing for me. I'm not sure
    if the `end` functionality is doing much for me since I have to explicitly
    activate it and I think snippets are largely better. Also the bracket an do/end
    swap doesn't work well. Finally I don't find a lot of the snippets useful.
    May just make my own Ruby module.

--- init.lua
+++ init.lua
@@ -73,3 +73,7 @@ end)
 events.connect(events.UNFOCUS, io.save_all_files)
 events.connect(events.BUFFER_BEFORE_SWITCH, io.save_all_files)
 events.connect(events.VIEW_BEFORE_SWITCH, io.save_all_files)
+
+snippets.ruby.exp = 'expect( %1(subject) ).to %0'
+snippets.ruby.let = 'let(%1) { %2 }%0'
+snippets.ruby.letb = 'let %1\n\t%0\nend'
