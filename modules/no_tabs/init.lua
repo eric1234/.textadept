1f8a67b8c791851fd3324919a26287338753d005
Eric Anderson <eric@bloodhub.com>
2022-10-31 10:50:38 -0400

    Attempt to improve odd opening of files due to this module
    It feels like a race condition. I will open a new file but it will think the
    old one is the active one still an close the new one instead of the old one.
    Adddinig a timeout seems to help but not perfectly. Even a couple seconds later
    it is confused on what is the current active view and close the one I opened.
    
    But since better will commit for now. I wonder if I should basically change this
    from "ignore active" to "ignore active and the one that triggered the event".

--- modules/no_tabs/init.lua
+++ modules/no_tabs/init.lua
@@ -1,16 +1,19 @@
-function contains(table, evaluator)
-  for _, item in ipairs(table) do
-    if evaluator(item) then return true end
-  end
-
-  return false
+local function buffer_in_use(buf)
+  return table.contains(_VIEWS, function(view)
+    return view.buffer.filename == buf.filename
+  end)
 end
 
-function close_unused_buffers()
-  for _, buffer in ipairs(_BUFFERS) do
-    local is_use = contains(_VIEWS, function(view) return view.buffer == buffer end)
-    if not is_use then buffer:close() end
-  end
+local function close_unused_buffers()
+  -- Cleanup doesn't have to happen right away and I think there are race
+  -- conditions if we do it too early after an event
+  timeout(2, function()
+    for _, buf in ipairs(_BUFFERS) do
+      if not buffer_in_use(buf) then
+        buf:close()
+      end
+    end
+  end)
 end
 
 ui.tabs = false
