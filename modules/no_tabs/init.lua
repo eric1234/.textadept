b434945e799b88b8fed3b5959b9a642b0c755931
Eric Anderson <afflicted.nimbly928@anonaddy.me>
2022-11-01 12:11:48 -0400

    Hopefully a more reliable version of this module.
    Just a timeout wasn't solving it as it often still messed with the buffers
    when it should not have. Tracked it down to two issues.
    
    I think the events being triggered are in their own thread meaning they often
    overlap causing problems with each other. This is solved by only running one
    at a time. If any others requested while one is running then ignore it. Still
    have a slight delay to give other modules a chance to "claim" a buffer before
    cleanup but it's not strictly necessary.
    
    The second issue is something internal to textadept causes the unrelated buffers
    on unrelated views to change. I've pinged on the project but can also work
    around it for now but just remembering the association and then ensuring that
    is the assocation when done.

--- modules/no_tabs/init.lua
+++ modules/no_tabs/init.lua
@@ -1,20 +1,50 @@
+-- Returns a function that ensures only once instance of it is active at
+-- a time. If another thread calls the same function it will be ignored.
+-- Also adds a slight delay to processing the function so the if another
+-- process will be using the unused buffer very shortly they can grab it before
+-- the cleanup.
+local function debounce(func)
+  local processing = false
+
+  return function()
+    if processing then return else processing = true end
+    timeout(0.1, function()
+      func()
+      processing = false
+    end)
+  end
+end
+
+-- Store what views are using what buffers. Then run the callback. Then ensure
+-- those associations from before as still valid. This is needed because
+-- sometimes closing a buffer seems to cause an unrelated view change to an
+-- unrelated buffer
+local function ensure_buffer_assignment(callback)
+  local usage = {}
+  for _, view in ipairs(_VIEWS) do
+    usage[view] = view.buffer
+  end
+
+  callback()
+
+  for view, buffer in pairs(usage) do
+    view:goto_buffer(buffer)
+  end
+end
+
 local function buffer_in_use(buf)
   return table.contains(_VIEWS, function(view)
     return view.buffer.filename == buf.filename
   end)
 end
 
-local function close_unused_buffers()
-  -- Cleanup doesn't have to happen right away and I think there are race
-  -- conditions if we do it too early after an event
-  timeout(2, function()
+local close_unused_buffers = debounce(function()
+  ensure_buffer_assignment(function()
     for _, buf in ipairs(_BUFFERS) do
-      if not buffer_in_use(buf) then
-        buf:close()
-      end
+      if not buffer_in_use(buf) then buf:close() end
     end
   end)
-end
+end)
 
 ui.tabs = false
 events.connect(events.FILE_OPENED, close_unused_buffers)
