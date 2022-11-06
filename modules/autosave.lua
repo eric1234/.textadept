1d84fcd71a05ca98cf0db32fcd54878f20bac55b
Eric Anderson <afflicted.nimbly928@anonaddy.me>
2022-11-06 11:59:10 -0600

    Improve comment

--- modules/autosave.lua
+++ modules/autosave.lua
@@ -1,4 +1,5 @@
 -- Auto-save all unsaved buffers when switching focus
+-- (different buffer, different view, different app)
 events.connect(events.UNFOCUS, io.save_all_files)
 events.connect(events.BUFFER_BEFORE_SWITCH, io.save_all_files)
 events.connect(events.VIEW_BEFORE_SWITCH, io.save_all_files)
