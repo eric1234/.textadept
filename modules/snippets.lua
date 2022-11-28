c8c48d6b6e328c673125f9b4758a245e243677de
Eric Anderson <eric@bloodhub.com>
2022-11-28 13:22:28 -0500

    Typos

--- modules/snippets.lua
+++ modules/snippets.lua
@@ -1,3 +1,3 @@
 snippets.ruby.exp = 'expect( %1(subject) ).to %0'
-snippets.ruby.let = 'let(%1) { %2 }%0'
-snippets.ruby.letb = 'let %1\n\t%0\nend'
+snippets.ruby.let = 'let(%1) { %0 }'
+snippets.ruby.letb = 'let %1 do\n\t%0\nend'
