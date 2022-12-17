e1a2eca3d0a71b917cc8a4710bf605d055f4bd70
Eric Anderson <afflicted.nimbly928@anonaddy.me>
2022-12-17 15:38:58 -0500

    Make quick-open work better when working in multiple projects

--- modules/quick_open.lua
+++ modules/quick_open.lua
@@ -4,12 +4,30 @@ io.quick_open_max = 10000
 -- Temp directory should not be included in default search
 table.insert(lfs.default_filter, '!/tmp$')
 
+-- Quick open will by default open a file descending from the project root of
+-- the current buffer. I think it will be more useful to filter for the project
+-- root of all open buffers. Most of the time all buffers will share the same
+-- root. But if not then it allows the current file to be replaced with a file
+-- from another project root you are currently working on.
+local function quick_open_all()
+  local paths = {}
+
+  for _, buf in ipairs(_BUFFERS) do
+    local root = io.get_project_root(buf.filename)
+    if not paths[root] then
+      table.insert(paths, root)
+    end
+  end
+
+  io.quick_open(paths)
+end
+
 -- Ctrl-Cmd/Alt-Shift-P is too many damn keys for so common an operation. Since
 -- we already have the up arrow for moving up I won't ever use Ctrl-P (in Mac
 -- cmd-P). Therefore stealing that keyboard shortcut. This also makes it the
--- same shortcut as Atom's fuzzy finder which is similiar functionality.
+-- same shortcut as Atom's fuzzy finder which is similar functionality.
 if OSX then
-  keys['cmd+p'] = io.quick_open
+  keys['cmd+p'] = quick_open_all
 else
-  keys['ctrl+p'] = io.quick_open
+  keys['ctrl+p'] = quick_open_all
 end
