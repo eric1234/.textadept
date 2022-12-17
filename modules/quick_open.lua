90e4c4202473d25535e73748a6d78203b6dbae4d
Eric Anderson <afflicted.nimbly928@anonaddy.me>
2022-12-17 15:28:36 -0500

    Use different key combo for quick open
    Ctrl-O is fine for Mac since Cmd-O does the file open but for PC it will replace
    the file open and I think we still want that for when we open files outside a
    project.
    
    Looked at Atom to see it's keyboard shortcut. Although already used there is
    a better alternative so we can steal it. Now we have both OS open and quick
    open for both operating systems.

--- modules/quick_open.lua
+++ modules/quick_open.lua
@@ -4,5 +4,12 @@ io.quick_open_max = 10000
 -- Temp directory should not be included in default search
 table.insert(lfs.default_filter, '!/tmp$')
 
--- Ctrl-Cmd-Shift-P is too many damn keys
-keys['ctrl+o'] = io.quick_open
+-- Ctrl-Cmd/Alt-Shift-P is too many damn keys for so common an operation. Since
+-- we already have the up arrow for moving up I won't ever use Ctrl-P (in Mac
+-- cmd-P). Therefore stealing that keyboard shortcut. This also makes it the
+-- same shortcut as Atom's fuzzy finder which is similiar functionality.
+if OSX then
+  keys['cmd+p'] = io.quick_open
+else
+  keys['ctrl+p'] = io.quick_open
+end
