edace0e622097403771ebb82409417f0abde476c
Eric Anderson <eric.anderson@invitahealth.com>
2023-07-24 09:49:33 -0400

    Better customize the find dialog

--- modules/find.lua
+++ modules/find.lua
@@ -6,8 +6,15 @@ function textadept.key_replace(old_func, new_func)
   end
 end
 
--- Assume currently selected text is what we want to search for
 textadept.key_replace(ui.find.focus, function()
-  ui.find.focus{find_entry_text = buffer:get_sel_text()}
-  buffer:set_selection(0, 0)
+  ui.find.focus()
+
+  local cur = buffer:get_sel_text()
+
+  -- User selected text. Assume that is what we search for
+  if cur ~= '' then
+    ui.find.find_entry_text = cur
+    ui.find.replace_entry_text = ''
+    buffer:set_selection(0, 0)
+  end
 end)
