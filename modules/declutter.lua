931ce377c9a23a6e487bb997cb7112dd2f10df31
Eric Anderson <afflicted.nimbly928@anonaddy.me>
2023-08-26 09:36:54 -0400

    Hide menu on non OSX
    I generally want to hide the menu everywhere. But on OSX it is at least
    non-instrusive and showing it there allows the keybindings to work better. On
    other platforms the keybindings bug is not present so just hide the menu.

--- modules/declutter.lua
+++ modules/declutter.lua
@@ -1,7 +1,9 @@
 -- Prefer keyboard shortcuts or a command palette over navigating a menu and
 -- the menu just adds clutter so disable it.
 events.connect(events.INITIALIZED, function()
---  textadept.menu.menubar = nil
+  -- Don't disable on OSX because of a bug
+  -- https://github.com/orbitalquark/textadept/issues/444
+  if not OSX then textadept.menu.menubar = nil end
 end)
 
 -- Code folding is not useful to me and too busy visually to leave enabled
